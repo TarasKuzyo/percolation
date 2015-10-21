ed96d17d1fc3fc8d1e3af20d42ffed7ebc953e9a
Taras Kuzyo <kuzyo.taras@gmail.com>
2015-10-22 01:00:03 +0300

    main program

---
+++ driver.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "definitions.h"
+
+
+int main(int argc, char **argv)
+{
+    srand(time(NULL));
+    
+    args cmd_args;
+    parse_options(argc, argv, &cmd_args);
+    
+    
+    grid *gd = allocate_grid(cmd_args.width, cmd_args.height);
+    run_percolation(gd, cmd_args.prob, cmd_args.recursive);
+
+    create_image(cmd_args.filename, gd, cmd_args.size);
+    //write_grid("out.txt", gd);
+
+    free_grid(gd);
+    
+    return 0;
+}
