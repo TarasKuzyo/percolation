55462a06347de8a10c47e158f3b613643d2b71fe
Taras Kuzyo <kuzyo.taras@gmail.com>
2015-10-22 00:57:21 +0300

    a header with shared definitions

---
+++ definitions.h
@@ -0,0 +1,59 @@
+#ifndef __DEFINITIONS_H__
+#define __DEFINITIONS_H__
+
+#define SITE_BLOCK 0
+#define SITE_OPEN  1
+#define SITE_FULL  2
+
+
+#define STR_BUF_SIZE 256
+
+
+typedef struct
+{
+    int width;
+    int height;
+    int **cells;
+
+} grid;
+
+
+typedef struct
+{
+    int width;
+    int height;
+    double prob;
+    double size;
+    int recursive;
+    char filename[STR_BUF_SIZE];
+} args;
+
+
+/* from utils.c */
+void free_grid(grid *gd);
+grid* allocate_grid(int width, int height);
+void write_grid(const char *filename, grid *gd);
+
+/* from percolation.c */
+
+void create_grid(grid *gd, double prob);
+
+void start_flow(grid *gd);
+int percolates(grid *gd);
+
+void flow_recursive(grid *gd);
+void flow_iterative(grid *gd);
+
+int run_percolation(grid *gd, double prob, int recursive_flag);
+
+/* from drawing.c */
+int create_image(const char *filename, grid *gd, double max_size);
+
+/* from parse_options.c */
+void parse_options(int argc, char **argv, args *arg);
+
+
+#endif /* __DEFINITIONS_H__ */
+
+
+
